5deffc3464be88f3d7e30cbdf0ba786c82bd4ebe
hmji <hmji@nansoft.co.kr>
2022-04-11 10:01:49 +0900

    Revert "운송장 출력 데이터 조회 수정"
    This reverts commit a3629246adad6d8ed3458176bfa8b924f4284b4c.

--- DeliTest/Models/Chunil/ResponseChunilModel.cs
+++ DeliTest/Models/Chunil/ResponseChunilModel.cs
@@ -11,28 +11,10 @@ namespace DeliTest.Models.Chunil
     /// </summary>
     public class ResponseChunilModel
     {
-        public string MAINNB { get; set; }
-        public string BWCODE { get; set; }
         public string JBDATE { get; set; }
-        public int CHASEQ { get; set; } = 0;
-        public string GDSORD { get; set; }
-        public int GDSQNT { get; set; } = 0;
-        public string SWNAME { get; set; }
-        public string SWTEL1 { get; set; }
-        public string SWZPCD { get; set; }
-        public string SWADDR { get; set; }
-        public string REMARK { get; set; }
-        public string SELCST { get; set; }
-        public string JUMNAM { get; set; }
-        public string JUMDAT { get; set; }
-        public string JUMNUM { get; set; }
-        public string PAYGBN { get; set; }
-        public string CPGKEY { get; set; }
-        public int CHARGE { get; set; } = 0;
-        public string MAINGB { get; set; }
-        public string PRTDAT { get; set; }
-
-        //Response
+        public string BWCODE { get; set; }
+        public int CHASEQ { get; set; }
+        public string MAINNB { get; set; }
         public string AREGBN { get; set; }
         public string GUBGBN { get; set; }
         public string SIDONG { get; set; }
@@ -42,10 +24,5 @@ namespace DeliTest.Models.Chunil
         public string MAINNM { get; set; }
         public string PRDATE { get; set; }
         public int CHASUM { get; set; } = 0;
-
-        public string REGDAT { get; set; }
-        public string PROGBN { get; set; }
-        public string PRODAT { get; set; }
-        public string ERRNAM { get; set; }
     }
 }
